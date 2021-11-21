b43e289ea9d3a10c87a95a464c5485bb86cbee30
emartinsson <erik.b.martinsson@gmail.com>
2021-11-21 21:37:48 +0100

    added javascript

--- scripts/main.js
+++ scripts/main.js
@@ -1,29 +1,44 @@
+let humanWins = 0;
+let compWins = 0;
+let draws = 0;
+let round = 0;
+
+
+function game(){
+    const buttons = document.querySelectorAll("button");
+    const choices = document.querySelector(".choices")
+    buttons.forEach((button) => button.addEventListener("click", () =>{
+        let playerChoice = button.id;
+        let computerChoice = computerSelection();
+        choices.textContent = `You selected ${playerChoice} and the computer went for ${computerChoice}`;
+        let result = playRound(playerChoice, computerChoice);
+        round++;
+        if (result === 1){
+            humanWins++;
+            choices.textContent += ". You win!";
+        } else if (result === -1){
+            compWins++;
+            choices.textContent += ". Computer win!";
+        } else {
+            draws++;
+            choices.textContent += ". It's a draw!";
+        }
+        updateScore();
+        if (humanWins === 5 || compWins === 5)  {
+            gameOver();
+        } 
+    }));
+
+}
+
+
 function computerSelection(){
     let choices = ["Rock", "Paper", "Scissors"];
     let randChoice = choices[Math.floor(Math.random()*choices.length)];
     return randChoice;
 }
 
-function playerSelection(){
-    let choices = ["Rock", "Paper", "Scissors"];
-    while (true){
-        let userChoice = prompt("Please select between " + choices.join(", "));
-        userChoice = userChoice.toLowerCase();
-        userChoice = userChoice.charAt(0).toUpperCase() + userChoice.slice(1);
-        for (let i = 0; i <choices.length; i++){
-            if (choices[i]===userChoice){
-                return userChoice;
-            }
-        }
-        alert("Wrong input.")
-    }
-}
-
-function playRound(){
-    let playerChoice = playerSelection();
-    let computerChoice = computerSelection();
-    console.log(`You selected ${playerChoice} and the computer went for ${computerChoice}`);
-
+function playRound(playerChoice, computerChoice){
     if (playerChoice === "Rock"){
         if (computerChoice === "Scissors"){
             return 1;
@@ -49,34 +64,26 @@ function playRound(){
     }
 }
 
-function game(){
-    let humanWins = 0;
-    let compWins = 0;
-    let draws = 0;
-    
-    for (let i = 1; i<=5; i++){
-        console.log("Round " + i);
-        let result = playRound();
-        if (result === 1){
-            humanWins++;
-            console.log("Human win human win human win");
-        } else if (result === -1){
-            compWins++;
-            console.log("Computer win computer win computer win");
-        } else {
-            draws++;
-            console.log("No one wins?! It's a draw!");
-        }
-        
-    }
-    let str = "";
-    console.log(`Human wins: ${humanWins}.\nComputer wins: ${compWins}.\nNumber of draws: ${draws}.`);
-    if (humanWins>compWins){
-        str+="Humans are superior to computers. The human wins the five round battle!";
-    }else if (compWins>humanWins){
-        str+="Like the terminator movie, computers are now superior. Humans lost the five round battle.";
-    } else {
-        str+="Humans and computers are equal, at least according to this game!";
-    }
-    console.log(str);
+
+const updateScore = () => {
+    const resultDiv = document.querySelector(".results");
+    resultDiv.textContent = `Human wins: ${humanWins}.\nComputer wins: ${compWins}.\nNumber of draws: ${draws}.`;
 }
+
+const gameOver = () => {
+    if (humanWins === 5){
+        alert("Human goes first to five games.")
+    } else {
+        alert("Computer goes first to five games.");
+    }
+    humanWins = 0;
+    compWins = 0;
+    draws = 0;
+    round = 0;
+    updateScore();
+    const choices = document.querySelector(".choices")
+    choices.textContent = "";
+}
+
+
+game();
